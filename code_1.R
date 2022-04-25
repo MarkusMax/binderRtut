8ea81658d94c51837f69f51a5efde0603f9c3c5d
Markus Knobloch <knobloch@leibniz-kis.de>
2022-04-25 16:21:42 +0200

    Added data vis

---
+++ code_1.R
@@ -0,0 +1,19 @@
+library(ggplot2)    
+library(ggmap)
+library(osmdata)
+
+# Getting data for Rotterdam
+rotterdam <- getbb('Rotterdam')
+setwd("/Users/knobloch/LocalWork/R_projects/Binder//bindRtut")
+df <- read.csv("rotter.csv")
+
+head(df)
+rott <- get_map(rotterdam, zoom = 11, source="osm", scale=1)
+
+ggm <- ggmap(rott, extent="device", legend="none")
+
+ggm <- ggm + geom_point(aes(x = lon,  y =lat),  data=df) +
+  ggtitle(paste("Location History Between: ", tail(df, 1)$datetime, "-", head(df, 1)$datetime))
+
+
+print(ggm)
